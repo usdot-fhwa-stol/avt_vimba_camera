d8cdf118a7d1bfffd5136a8fd7fb7373157c3bfe
jtbaird <john.baird@leidos.com>
2020-03-19 13:56:44 -0400

    updated for develop option

--- docker/build-image.sh
+++ docker/build-image.sh
@@ -39,9 +39,9 @@ while [[ $# -gt 0 ]]; do
             PUSH=true
             shift
             ;;
-        -t|--test)
-            USERNAME=usdotfhwastoltest
-            COMPONENT_VERSION_STRING=test
+        -d|--develop)
+            USERNAME=usdotfhwastoldev
+            COMPONENT_VERSION_STRING=develop
             shift
             ;;
     esac
@@ -56,7 +56,7 @@ echo "Final image name: $USERNAME/$IMAGE:$COMPONENT_VERSION_STRING"
 
 cd ..
 if [[ $COMPONENT_VERSION_STRING = "test" ]]; then
-    sed "s|usdotfhwastol|usdotfhwastoltest|g; s|:[0-9]*\.[0-9]*\.[0-9]*|:$COMPONENT_VERSION_STRING|g; s|checkout.sh|checkout.sh -t|g" \
+    sed "s|usdotfhwastol|$USERNAME|g; s|:[0-9]*\.[0-9]*\.[0-9]*|:$COMPONENT_VERSION_STRING|g; s|checkout.sh|checkout.sh -d|g" \
         Dockerfile | docker build -f - --no-cache -t $USERNAME/$IMAGE:$COMPONENT_VERSION_STRING \
         --build-arg VERSION="$COMPONENT_VERSION_STRING" \
         --build-arg VCS_REF=`git rev-parse --short HEAD` \
--- docker/checkout.sh
+++ docker/checkout.sh
@@ -23,7 +23,7 @@ dir=~
 while [[ $# -gt 0 ]]; do
       arg="$1"
       case $arg in
-            -t|--test)
+            -d|--develop)
                   BRANCH=develop
                   shift
             ;;
@@ -36,7 +36,7 @@ while [[ $# -gt 0 ]]; do
 done
 
 if [[ "$BRANCH" = "develop" ]]; then
-      git clone https://github.com/usdot-fhwa-stol/CARMAMsgs.git ${dir}/src/CARMAMsgs --branch $BRANCH --depth 1
+      git clone https://github.com/usdot-fhwa-stol/CARMAMsgs.git ~/src/CARMAMsgs --branch $BRANCH --depth 1
 else
       git clone https://github.com/usdot-fhwa-stol/CARMAMsgs.git ${dir}/src/CARMAMsgs --branch CARMAMsgs_1.1.0 --depth 1
 fi
